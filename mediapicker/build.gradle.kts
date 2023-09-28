f11c001d4644c7eb6d5643e49cb4885831594cd3
LuisAngelDD <94653501+LuisAngelDD@users.noreply.github.com>
2023-09-28 13:46:26 -0600

    Update build.gradle.kts

--- mediapicker/build.gradle.kts
+++ mediapicker/build.gradle.kts
@@ -73,7 +73,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.luisangeldd"
                 artifactId = "MediaPicker"
-                version = "1.0.0"
+                version = "1.0"
             }
         }
     }
